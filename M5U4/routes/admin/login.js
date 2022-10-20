8fcc486aa221656882c581a5923438f34bda970b
Matias Galeano <matiasgala@hotmail.com>
2022-10-19 21:03:58 -0300

    prueba fallida m5u4

---
+++ M5U4/routes/admin/login.js
@@ -0,0 +1,35 @@
+var express = require('express');
+var router = express.Router();
+var usuariosModel = require('./../../models/usuariosModel');
+
+
+router.get('/', function(req, res, next) {
+  res.render('admin/login',{
+    layout: 'admin/layout'
+  });
+});
+
+router.post('/', async (req, res, next) =>{
+    try {
+        var usuario = req.body.usuario;
+        var password = req.body.password;
+
+        //console.log(req.body);//
+
+        var data = await usuariosModel.getUserByUsernameAndPassword(usuario, password);
+
+        if (data != undefined) {
+            res.redirect('/admin/novedades');
+        } else {
+            res.render('admin/login', {
+                layout: 'admin/layout',
+                error: true
+            });
+        }
+    } catch (error){
+        //console.log(error);
+    }
+});
+
+
+module.exports = router;
